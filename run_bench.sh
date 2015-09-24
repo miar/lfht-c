6cd8b7f8bda64d5234b32929025230c96a148c5c
Miguel Areias <miguel-areias@dcc.fc.up.pt>
2015-09-24 16:11:46 +0100

    run_bench -> compiling code and check assembly code

--- run_bench.sh
+++ run_bench.sh
@@ -5,8 +5,12 @@
 rm bench.out
 #gcc  -Wall  -Wmissing-prototypes -Wmissing-declarations bench.c -g -o bench.out
 
+#compile and check assembly code
+
+gcc -c -g -Wa,-a,-ad bench.c -o bench.out > foo.lst
+
 # compile on with gcc 
-clang -Wall bench.c -o bench.out
+#clang -Wall bench.c -o bench.out
 
 #and run for the moment...
 
