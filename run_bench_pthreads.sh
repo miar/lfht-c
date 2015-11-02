e9e6d7eb4e0b0af85cf2389eafbd2fbaf6650aa0
Miguel Areias <miguel-areias@dcc.fc.up.pt>
2015-11-02 11:49:27 +0000

    bench_pthreads.sh

---
+++ run_bench_pthreads.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# compile on with gcc 
+
+rm bench.out
+gcc  -Wall -Wmissing-prototypes -Wmissing-declarations bench_pthreads.c -lpthread -g -o bench.out
+
+#compile and check assembly code
+
+#gcc -c -g -Wa,-a,-ad -lpthread bench_pthreads.c -o bench.out > foo.lst
+
+# compile on with gcc 
+#clang -Wall -lpthread bench_pthreads.c -o bench.out
+
+#and run for the moment...
+
+./bench.out
