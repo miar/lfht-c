d76f79d4d1dc321b9b1dc53936a726b8f35e46c7
Miguel Areias <miguel-areias@dcc.fc.up.pt>
2015-11-16 17:23:39 +0000

    swig stuff

--- misc/swig/lfht/RunMe.java
+++ misc/swig/lfht/RunMe.java
@@ -16,11 +16,21 @@ public class RunMe {
 	//	int f = e.fact(3);
 	//	    System.out.println("Hello, World " + e.fact(3));
 	
+	//e.LFHT_ThreadEnvPtr tenv;
         e.dic_create_init_env();
-	e.dic_check_insert_key(0, 0, e.dic_create_init_thread_env(0));
+
+	SWIGTYPE_p_LFHT_ThreadEnvPtr tenv1 = e.dic_create_init_thread_env(1);
+	SWIGTYPE_p_LFHT_ThreadEnvPtr tenv2 = e.dic_create_init_thread_env(2);
+
+	e.dic_check_insert_key(0, 0, tenv1);
+	e.dic_check_insert_key(1, 2, tenv2);
+
 	e.dic_show_state("stdout");
 	e.dic_abolish_all_keys();
 	e.dic_show_state("stdout");
 
+	e.dic_kill_thread_env(1);
+	e.dic_kill_thread_env(2);
+	e.dic_kill_env();
     }    
 }
--- misc/swig/lfht/lfht.i
+++ misc/swig/lfht/lfht.i
@@ -5,6 +5,21 @@
 
    %}
 
+
+
+
+/*
+typedef struct LFHT_ThreadEnvironment {
+  void  *mem_ref;              
+  void  *mem_ref_next;         
+  void  *unused_node;          
+  void **unused_bucketArray;   
+  long   number_of_operations; 
+  void  *cache_line_padding[3];
+} *LFHT_ThreadEnvPtr;
+*/
+
+
 extern dic_ptr dic_check_insert_key(long, long, LFHT_ThreadEnvPtr);
 
 extern dic_ptr dic_check_delete_key(long, long, LFHT_ThreadEnvPtr);
@@ -22,3 +37,5 @@ extern void dic_create_init_env(void);
 extern LFHT_ThreadEnvPtr dic_create_init_thread_env(int);
 
 extern void dic_kill_thread_env(int);
+
+extern void dic_kill_env(void);
